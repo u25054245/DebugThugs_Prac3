e0c46bf21491584018045517825bc26f6f0466a1
u25265718-JR <u25265718@tuks.co.za>
2026-08-31 17:02:09 +0200

    Update MedicalTent.h

--- MedicalTent.h
+++ MedicalTent.h
@@ -13,18 +13,19 @@ using namespace std;
 class MedicalTent : public MarathonEvent, public Observer{
     private:
         int alertLevel;
-        bool operational;
-        int runnersServed;
+       
+        
     public:
         void update(Subject* subject);
         void open() override;
         void close() override;
         void reportStatus() const override;
         void getCapacity() const override;
-        int getRemainingCups();
-        void refill();
+        int getAlertLevel() const;
+        void setAlertLevel(int level);
+        
 
-        ~WaterTable();
-        WaterTable();
+        ~MedicalTent();
+        MedicalTent();
 };
 #endif
